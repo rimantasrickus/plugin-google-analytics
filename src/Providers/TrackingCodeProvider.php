266dcbd72fe4abe0a0d0906141e426442a76b0d5
Felix Dausch <felix.dausch@plentymarkets.com>
2017-02-24 10:59:51 +0100

    ADD tracking for transactions and basket items from order confirmation page

--- src/Providers/TrackingCodeProvider.php
+++ src/Providers/TrackingCodeProvider.php
@@ -2,19 +2,20 @@
 
 namespace GoogleAnalytics\Providers;
 
+use IO\Services\CustomerService;
+use IO\Services\OrderService;
 use Plenty\Modules\Basket\Contracts\BasketRepositoryContract;
 use Plenty\Modules\Basket\Models\Basket;
+use Plenty\Modules\Frontend\Services\AccountService;
 use Plenty\Plugin\ConfigRepository;
 use Plenty\Plugin\Templates\Twig;
 
 
 class TrackingCodeProvider
 {
-    public function call(   Twig $twig,
-                            ConfigRepository $configRepository,
-                            BasketRepositoryContract $basketRepositoryContract,
-                            $args)
+    public function call( Twig $twig, CustomerService $customerService )
     {
-        return $twig->render('GoogleAnalytics::GoogleAnalyticsTrackingCode');
+
+        return $twig->render('GoogleAnalytics::GoogleAnalyticsTrackingCode', ['data' => $customerService->getLatestOrder()]);
     }
 }
