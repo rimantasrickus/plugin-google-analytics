1de589a1a40db8fb494d2317b96a95ac1cbbbd3a
Felix Dausch <felix.dausch@plentymarkets.com>
2019-10-31 14:45:50 +0100

    ADD cookie configs

--- src/Providers/GoogleAnalyticsServiceProvider.php
+++ src/Providers/GoogleAnalyticsServiceProvider.php
@@ -5,6 +5,7 @@ namespace GoogleAnalytics\Providers;
 use Plenty\Modules\Frontend\Session\Storage\Contracts\FrontendSessionStorageFactoryContract;
 use Plenty\Modules\Order\Events\OrderCreated;
 use Plenty\Modules\Webshop\Consent\Contracts\ConsentRepositoryContract;
+use Plenty\Plugin\ConfigRepository;
 use Plenty\Plugin\Events\Dispatcher;
 use Plenty\Plugin\ServiceProvider;
 use Plenty\Plugin\Templates\Twig;
@@ -23,6 +24,9 @@ class GoogleAnalyticsServiceProvider extends ServiceProvider
         /** @var ConsentRepositoryContract $consentRepository */
         $consentRepository = pluginApp(ConsentRepositoryContract::class);
 
+        /** @var ConfigRepository $config */
+        $config = pluginApp(ConfigRepository::class);
+
         $consentRepository->registerConsent(
             'googleAnalytics',
             'GoogleAnalytics::GoogleAnalytics.consentLabel',
@@ -31,7 +35,9 @@ class GoogleAnalyticsServiceProvider extends ServiceProvider
                 'provider' => 'GoogleAnalytics::GoogleAnalytics.consentProvider',
                 'lifespan' => 'GoogleAnalytics::GoogleAnalytics.consentLifespan',
                 'policyUrl' => 'GoogleAnalytics::GoogleAnalytics.consentPolicyUrl',
-                'group' => 'tracking',
+                'group' => $config->get('GoogleAnalytics.consentGroup', 'tracking'),
+                'necessary' => $config->get('GoogleAnalytics.consentNecessary') === 'true',
+                'isOptOut' => $config->get('GoogleAnalytics.consentOptOut') === 'true',
                 'cookieNames' => ['_ga', '_gid', '_gat']
             ]
         );
