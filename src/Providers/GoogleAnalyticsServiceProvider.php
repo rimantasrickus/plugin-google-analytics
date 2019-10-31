60b9d9f530f625f0fa8fd0b3278bc0f0d932dfc7
Felix Dausch <felix.dausch@plentymarkets.com>
2019-10-31 11:36:10 +0100

    Register cookies

--- src/Providers/GoogleAnalyticsServiceProvider.php
+++ src/Providers/GoogleAnalyticsServiceProvider.php
@@ -4,6 +4,7 @@ namespace GoogleAnalytics\Providers;
 
 use Plenty\Modules\Frontend\Session\Storage\Contracts\FrontendSessionStorageFactoryContract;
 use Plenty\Modules\Order\Events\OrderCreated;
+use Plenty\Modules\Webshop\Consent\Contracts\ConsentRepositoryContract;
 use Plenty\Plugin\Events\Dispatcher;
 use Plenty\Plugin\ServiceProvider;
 use Plenty\Plugin\Templates\Twig;
@@ -19,6 +20,21 @@ class GoogleAnalyticsServiceProvider extends ServiceProvider
      */
     public function register()
     {
+        /** @var ConsentRepositoryContract $consentRepository */
+        $consentRepository = pluginApp(ConsentRepositoryContract::class);
+
+        $consentRepository->registerConsent(
+            'googleAnalytics',
+            'GoogleAnalytics::GoogleAnalytics.consentLabel',
+            [
+                'description' => 'GoogleAnalytics::GoogleAnalytics.consentDescription',
+                'provider' => 'GoogleAnalytics::GoogleAnalytics.consentProvider',
+                'lifespan' => 'GoogleAnalytics::GoogleAnalytics.consentLifespan',
+                'policyUrl' => 'GoogleAnalytics::GoogleAnalytics.consentPolicyUrl',
+                'group' => 'tracking',
+                'cookieNames' => ['_ga', '_gid', '_gat']
+            ]
+        );
     }
 
     /**
