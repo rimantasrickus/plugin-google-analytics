3fb8a71e2fdb1282f3c39ea7af8f88a61f373422
Rimantas R <rimantas.rickus@hashtages.com>
2022-12-15 13:52:22 +0200

    add logs

--- src/Providers/TrackingCodeProvider.php
+++ src/Providers/TrackingCodeProvider.php
@@ -5,17 +5,25 @@ namespace GoogleAnalytics\Providers;
 use Plenty\Modules\Frontend\Session\Storage\Contracts\FrontendSessionStorageFactoryContract;
 use Plenty\Plugin\Templates\Twig;
 
-
 class TrackingCodeProvider
 {
-    public function call( Twig $twig )
+    public function call(Twig $twig)
     {
         /** @var FrontendSessionStorageFactoryContract $sessionStorage */
         $sessionStorage = pluginApp(FrontendSessionStorageFactoryContract::class);
 
+        $logger = pluginApp(LoggerFactory::class)->getLogger('GoogleAnalytics', __METHOD__);
+        $logger->error('before unset', [
+            'plugin' => $sessionStorage->getPlugin()
+        ]);
+
         $enableTracking = $sessionStorage->getPlugin()->getValue('GA_TRACK_ORDER') === 1;
 
         $sessionStorage->getPlugin()->unsetKey('GA_TRACK_ORDER');
+
+        $logger->error('after unset', [
+            'plugin' => $sessionStorage->getPlugin()
+        ]);
 
         return $twig->render(
             'GoogleAnalytics::GoogleAnalyticsTrackingCode',
